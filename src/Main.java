fb5a3ad11c1b0609607791d207c8ae59d53dd9cf
Amit Nivargi <110668796+Amitnivargi@users.noreply.github.com>
2023-02-01 19:39:30 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Bank bank = new Bank();
         Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank();
+
+        // Continuously read inputs from the user
         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(" ");
-            if (parts.length == 0) {
-                continue;
-            }
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ");
             String command = parts[0];
 
+            // Trim any leading or trailing whitespaces from the account code
+            String code = parts[1].trim();
+
+            // Use a switch statement to handle different commands
             switch (command) {
                 case "CREATE":
-                    bank.createAccount(parts[1], parts[2]);
+                    // Call the createAccount method and pass the account code and name as arguments
+                    bank.createAccount(code, parts[2]);
                     break;
                 case "DEPOSIT":
-                    bank.depositAmount(parts[1], Integer.parseInt(parts[2]));
+                    // Call the depositAmount method and pass the account code and amount as arguments
+                    bank.depositAmount(code, Integer.parseInt(parts[2]));
                     break;
                 case "WITHDRAW":
-                    bank.withdrawAmount(parts[1], Integer.parseInt(parts[2]));
+                    // Call the withdrawAmount method and pass the account code and amount as arguments
+                    bank.withdrawAmount(code, Integer.parseInt(parts[2]));
                     break;
                 case "BALANCE":
-                    bank.showBalance(parts[1]);
+                    // Call the showBalance method and pass the account code as an argument
+                    bank.showBalance(code);
                     break;
                 default:
+                    // If the command is not recognized, print an error message
                     System.out.println("Invalid command. Try again.");
                     break;
             }
         }
+
+        // Close the scanner after all inputs have been read
         scanner.close();
     }
+
 }
